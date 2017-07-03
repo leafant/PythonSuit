a44ebcd46a8854b516de14a4cb3cf449e7cd2235
leafant <guoxiaoyestar@163.com>
2017-07-03 15:04:03 +0800

    Update Regex_ReplaceStringInfile.py

--- Regex_ReplaceStringInfile.py
+++ Regex_ReplaceStringInfile.py
@@ -1,5 +1,6 @@
         f1 = open('C:\Sublines\DataFiles\*.osc', 'r')            
-        lines = f1.read()        r = r"<KEY KEYINDEX=\"0\">.*<\/KEY>"        
+        lines = f1.read()        
+        r = r"<KEY KEYINDEX=\"0\">.*<\/KEY>"        
         replace = re.sub(r, '<KEY KEYINDEX="0">1111114B4145534B45592215441554B4</KEY>', lines)        
         f1.close()        
         f2 = open('C:\Sublines\DataFiles\*.osc', 'a')        
