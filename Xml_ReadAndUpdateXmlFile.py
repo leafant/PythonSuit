5ea70b572af6e07418b94b5e66e5f0454fa46f07
leafant <guoxiaoyestar@163.com>
2017-07-03 15:04:41 +0800

    Update Xml_ReadAndUpdateXmlFile.py

--- Xml_ReadAndUpdateXmlFile.py
+++ Xml_ReadAndUpdateXmlFile.py
@@ -1,11 +1,14 @@
 def updateCcaSlotInScdFile(self, fileName = '', slot = '',  indexOf3AesPesk = '', newSn = False):    
-    '''    Will update values of slot, sn, ua in specified scd file. Only one card  exist in the file.    Since we can't upload duplicated sn in Db, so the method will also update sn and ua when change the value of Slot.    '''    
+    '''    Will update values of slot, sn, ua in specified scd file. Only one card  exist in the file.    
+    Since we can't upload duplicated sn in Db, so the method will also update sn and ua when change the value of Slot.    
+    '''    
     if "\\" not in fileName:        
         rootFolder = Cfg.getConfigurationItem('AppServer.TestRoot')        
         fileName =  os.path.join(rootFolder,  'DataFiles', fileName)    
     tree = ElementTree()    tree.parse(fileName)    
     root = tree.getroot()             
-    nodes = root.findall("PROFILE/CLIENT_DEVICE_DATA/GLOBAL_PK")        
+    nodes = root.findall("PROFILE/CLIENT_DEVICE_DATA/GLOBAL_PK")
+    
     ## Update the slot and index value. Only 3aes Pesk support Pesk.    
     for node in nodes:        
         print node.get("slot")        
